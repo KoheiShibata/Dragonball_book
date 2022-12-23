5771dd15f55da31dc5fb3ee040b5004dd934d137
matsudashogo <muvs1018@gmail.com>
2022-12-23 13:23:25 +0900

    #8 ルーティング仮修正

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\ZukanController;
+use App\Http\Controllers\SeasonController;
+use App\Http\Controllers\TribeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,26 +21,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/seasons", [App\Http\Controllers\SeasonController::class, "seasons"]);
-Route::post("/season_create", [App\Http\Controllers\SeasonController::class, "create"]);
-Route::post("/season_edit/{id}", [App\Http\Controllers\SeasonController::class, "edit"]);
-Route::get("/season_delete/{id}", [App\Http\Controllers\SeasonController::class, "delete"]);
+Route::controller(SeasonController::class)->group(function(){
+    Route::get("/seasons", "seasons");
+    Route::post("/season_create", "create");
+    Route::post("/season_edit/{id}", "edit");
+    Route::get("/season_delete/{id}", "delete");
+});
 
-Route::get("/tribes", [App\Http\Controllers\TribeController::class, "tribes"]);
-Route::post("/tribe_create", [App\Http\Controllers\TribeController::class, "create"]);
-Route::post("/tribe_edit", [App\Http\Controllers\TribeController::class, "edit"]);
-Route::get("/tribe_delete/{id}", [App\Http\Controllers\TribeController::class, "delete"]);
+Route::controller(TribeController::class)->group(function(){
+    Route::get("/tribes", "tribes");
+    Route::post("/tribe_create", "create");
+    Route::post("/tribe_edit",  "edit");
+    Route::get("/tribe_delete/{id}", "delete");
+});
 
-Route::get("/character_create",[App\Http\Controllers\CharacterController::class, "character_create"]);
-Route::post("/character_register",[App\Http\Controllers\CharacterController::class, "character_register"]);
-Route::get("/character_list",[App\Http\Controllers\CharacterController::class, "character_list"]);
-Route::get("/character_edit/{id}",[App\Http\Controllers\CharacterController::class, "edit"]);
-Route::post("/character_update",[App\Http\Controllers\CharacterController::class, "update"]);
-Route::get("/character_delete/{id}",[App\Http\Controllers\CharacterController::class, "delete"]);
+Route::controller(CharacterController::class)->group(function(){
+    Route::get("/character_create", "character_create");
+    Route::post("/character_register", "character_register");
+    Route::get("/character_list", "character_list");
+    Route::get("/character_edit/{id}", "edit");
+    Route::post("/character_update", "update");
+    Route::get("/character_delete/{id}",  "delete" );
+});
 
 
-Route::get("/dragonball_zukan",[App\Http\Controllers\ZukanController::class, "dragonball_zukan"]);
-Route::get("/character_detail/{id}",[App\Http\Controllers\ZukanController::class, "character_detail"]);
+Route::controller(ZukanController::class)->group(function(){
+    Route::get("/dragonball_zukan", "dragonball_zukan");
+    Route::get("/character_detail/{id}", "character_detail");
+});
 
 // Route::get("/character_create", [App\Http\Controllers\CharacterController::class, "character_create"]);
 // Route::post("/create", [App\Http\Controllers\CharacterController::class, "create"]);
