f93b8ac55f09f6cb7dbd3eb1f806404c6dcee88b
KoheiShibata <ko.bsk116@gmail.com>
2023-04-04 20:55:13 +0900

    #30 googleフォームデータ取得api

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::controller(CharacterController::class)->prefix("characters")->group(function () {
     Route::get("/{token}", "characterData");
+    Route::put("/{token}", "update");
 });
